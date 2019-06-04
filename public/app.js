bfa6a57146ff9b9e3320a6d6ef49929ce0b03013
All <volodya22all@gmail.com>
2019-06-04 14:29:06 +0300

    Update orders input field

--- public/app.js
+++ public/app.js
@@ -17,6 +17,8 @@ function add_to_cart(id)
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
 
+	update_orders_input();
+
 	alert('Items in your cart: ' + cart_get_number_of_items())
 }
 
@@ -56,6 +58,12 @@ function cart_get_orders()
 	}
 
 	return orders;
+}
+
+function update_orders_input()
+{
+	var orders = cart_get_orders();
+	$('#orders_input').val(orders);
 }
 
 
