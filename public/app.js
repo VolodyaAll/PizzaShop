562bdbcdfa01fa5347571255eef0f180ae78530b
All <volodya22all@gmail.com>
2019-06-04 10:38:36 +0300

    Add item counter

--- public/app.js
+++ public/app.js
@@ -12,9 +12,17 @@ function something()
 function add_to_cart(id)
 {	
 	var key = 'product_' + id;
+	var total = 0;
+
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
+
+	for ( var i = 0, len = localStorage.length; i < len; ++i ) {
+  		total = total + localStorage.getItem( localStorage.key( i ) )*1;
+	}
+
+	alert(total);
 }
 
 
